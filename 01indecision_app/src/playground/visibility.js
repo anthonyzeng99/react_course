97448bdc5e8c5e0fa6ec01c4906b4b5c1ff65f60
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-30 12:47:05 -0400

    finished lesson 21

---
+++ 01indecision_app/src/playground/visibility.js
@@ -0,0 +1,26 @@
+const app = {
+  title: 'Visibility Toggle',
+  details: <p>Here are the details of the app</p>,
+  visible: true
+}
+
+const onVisible = () => {
+  app.visible = !app.visible;
+  renderApp();
+}
+
+const appRoot = document.getElementById('app');
+
+const renderApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      <button onClick={onVisible}>{app.visible ? 'Hide details' : 'Show details'}</button>
+      {app.visible && app.details}
+    </div>
+  );
+
+  ReactDOM.render(template,appRoot);
+}
+
+renderApp();
