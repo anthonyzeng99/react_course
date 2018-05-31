9f385aa7419e01fadd4248310689a86bbc35dad8
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-31 19:57:29 -0400

    finished lesson 54

--- 01indecision_app/src/app.js
+++ 01indecision_app/src/app.js
@@ -2,7 +2,7 @@ import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = React.createElement('p', {}, 'testing 123');
+const template = <p>This is JSX from Webpack</p>
 
 ReactDOM.render(template, document.getElementById('app'));
 
