ecd52487fddac940d718dcca9d272feeda78cda7
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-31 11:37:32 -0400

    finished lesson 41

--- 01indecision_app/src/app.js
+++ 01indecision_app/src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     }
   }
   handleDeleteOptions() {
@@ -39,7 +39,7 @@ class IndecisionApp extends React.Component {
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -56,13 +56,21 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+  options: ['First Thing']
+};
+
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   );
+}
+
+Header.defaultProps = {
+  title: 'Indecision'
 }
 
 const Action = (props) => {
