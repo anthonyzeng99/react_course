5305163e5ba8312586fa79acfc59a6fe7baa5519
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-31 14:11:47 -0400

    finished lesson 45

--- 01indecision_app/src/app.js
+++ 01indecision_app/src/app.js
@@ -10,10 +10,22 @@ class IndecisionApp extends React.Component {
     }
   }
   componentDidMount() {
-    console.log('fetching data');
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({options}));
+      }
+    } catch (e) {
+
+    }
   }
   componentDidUpdate(prevProps, prevState) {
-    console.log('saving data');
+    if (prevState.options.length != this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
   }
   componentWillUnmount() {
     console.log('componentWillUnmount');
@@ -100,6 +112,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove all</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
       {
         props.options.map((option) => (
           <Option
@@ -142,7 +155,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
     this.setState(() => ({error}));
 
-    event.target.elements.option.value = '';
+    if (!error) {
+      event.target.elements.option.value = '';
+    }
+
   }
   render() {
     return (
