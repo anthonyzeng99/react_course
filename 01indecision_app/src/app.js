46340a3908ee321382271b0fdf38edf2220d938b
Anthony Zeng <anthonyzeng99@gmail.com>
2018-05-30 20:38:57 -0400

    finished lesson 29

--- 01indecision_app/src/app.js
+++ 01indecision_app/src/app.js
@@ -27,19 +27,28 @@ class Header extends React.Component  {
 }
 
 class Action extends React.Component  {
+  handlePick() {
+    alert('handlePick');
+  }
+
   render () {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('handleRemoveAll');
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove all</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option}/>)
         }
@@ -59,10 +68,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(event) {
+    event.preventDefault();
+    const option = event.target.elements.option.value.trim();
+    if (option) {
+      alert(option);
+    }
+    event.target.elements.option.value = '';
+  }
+
   render() {
     return (
       <div>
-        <button>Add option</button>
+      <form onSubmit={this.handleAddOption}>
+        <input type="text" name="option" autoComplete="off"/>
+        <button>Add Option</button>
+      </form>
       </div>
     );
   }
