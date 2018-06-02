4efb45b95d610e3eb2590a6cc816d26a702cb0ef
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-02 14:30:12 -0400

    finished lesson 76

---
+++ 02expensify/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const publicPath = path.join(__dirname, 'public');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: publicPath,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: publicPath
+  }
+}
