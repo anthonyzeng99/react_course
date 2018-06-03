1a4310f9336d2634b25d3f35df6480c8aa153efa
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-03 00:56:38 -0400

    finished lesson 78

--- 02expensify/src/app.js
+++ 02expensify/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -29,14 +29,21 @@ const HelpPage = () => (
   </div>
 )
 
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+)
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-      <Route path="/create" component={AddExpensePage} exact={true}/>
-      <Route path="/edit" component={EditExpensePage} exact={true}/>
-      <Route path="/help" component={HelpPage} exact={true}/>
-    </div>
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
