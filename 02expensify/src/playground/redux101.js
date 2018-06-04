9bebb4812f83b4ebc0d70c629a70992d5a0855b0
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-04 17:32:58 -0400

    finished lesson 87

--- 02expensify/src/playground/redux101.js
+++ 02expensify/src/playground/redux101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
       case 'INCREMENT':
+        const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
         return {
-          count: state.count + 1
+          count: state.count + incrementBy
         }
       case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
         return {
-          count: state.count - 1
+          count: state.count - decrementBy
+        }
+      case 'SET':
+        return {
+          count: action.count
         }
       case 'RESET':
         return {
@@ -19,9 +25,14 @@ const store = createStore((state = { count: 0 }, action) => {
     }
 });
 
+store.subscribe(() => {
+console.log(store.getState());
+});
+
 console.log(store.getState());
 store.dispatch({
-  type: "INCREMENT"
+  type: "INCREMENT",
+  incrementBy: 5
 });
 
 store.dispatch({
@@ -36,4 +47,12 @@ store.dispatch({
   type: "DECREMENT"
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10
+});
+
+store.dispatch ({
+  type: "SET",
+  count: 101
+})
