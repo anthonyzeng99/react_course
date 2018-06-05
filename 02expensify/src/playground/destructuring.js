db329ee5e664c2a5d7e02a118aac84a64fb77f7f
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-04 20:57:19 -0400

    finished lesson 89

--- 02expensify/src/playground/destructuring.js
+++ 02expensify/src/playground/destructuring.js
@@ -1,3 +1,5 @@
+//Object
+
 // const person = {
 //   name: 'Anthony',
 //   age: 26,
@@ -17,15 +19,28 @@
 // if (city && temperature) {
 //   console.log(`It's ${temperature} in ${city}`);
 // }
+//
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// }
+//
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+//
+// console.log(publisherName);
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-}
+//Array
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
+const address = ['123 Main St', 'Anytown', 'Anystate', '12345'];
 
-console.log(publisherName);
+const [, city, state = 'NY'] = address;
+
+console.log(`You are in ${city} ${state}.`);
+
+const menu = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [item, ,mediumPrice] = menu;
+
+console.log(`A medium ${item} costs ${mediumPrice}`)
