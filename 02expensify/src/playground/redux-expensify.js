3a7906b2d46f5fde7d9b0b5f085f84998f81a1eb
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-04 22:58:50 -0400

    finished lesson 92

--- 02expensify/src/playground/redux-expensify.js
+++ 02expensify/src/playground/redux-expensify.js
@@ -1,5 +1,40 @@
 import { createStore, combineReducers } from 'redux';
 
+// Expenses reducer
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = [expensesReducerDefaultState] , action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Filters reducers
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+// Store creation
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [{
     id: 'abc',
