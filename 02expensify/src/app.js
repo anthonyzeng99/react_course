dfe23b3146f032abd58a6db8c9f1088bdcf795cd
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-05 15:42:36 -0400

    finished lesson 101

--- 02expensify/src/app.js
+++ 02expensify/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider }  from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -14,10 +14,16 @@ const store = configureStore();
 
 store.dispatch(addExpense({description: 'water bill', amount: 40000, createdAt:50000}));
 store.dispatch(addExpense({description: 'gas bill', amount: 20000, createdAt:100000}));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
