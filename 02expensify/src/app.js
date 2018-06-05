3a23c8fdc9456c5874220cbf3cb44a087204f56d
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-05 13:35:22 -0400

    finished lesson 99

--- 02expensify/src/app.js
+++ 02expensify/src/app.js
@@ -5,4 +5,19 @@ import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+import configureStore from './store/configureStore';
+import { addExpense, editExpense, removeExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'water bill', amount: 40000, createdAt:50000}));
+store.dispatch(addExpense({description: 'gas bill', amount: 20000, createdAt:100000}));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ 02expensify/src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers } from 'redux';
+
+// Reducers
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
+
+
+export default () => {
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer
+    })
+  );
+
+  return store;
+
+};
