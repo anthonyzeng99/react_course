77793f67890abbb04f0c59cc2f70d002e3cca737
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-07 17:54:14 -0400

    finished lesson 121

--- 02expensify/src/app.js
+++ 02expensify/src/app.js
@@ -4,6 +4,7 @@ import { Provider }  from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import "react-dates/lib/css/_datepicker.css"
 
 import configureStore from './store/configureStore';
 import { addExpense, editExpense, removeExpense } from './actions/expenses';
