5d03eb252e5688bada94d20388543ee2a640d336
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-07 23:40:57 -0400

    finished lesson 133

--- 02expensify/src/app.js
+++ 02expensify/src/app.js
@@ -13,16 +13,6 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'water bill', amount: 30000, createdAt:400000000}));
-store.dispatch(addExpense({description: 'gas bill', amount: 10000, createdAt:2000}));
-store.dispatch(addExpense({description: 'electric bill', amount: 20000, createdAt:3000}));
-store.dispatch(addExpense({description: 'rent', amount: 195000, createdAt:1000}));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
--- 02expensify/webpack.config.js
+++ 02expensify/webpack.config.js
@@ -2,25 +2,29 @@ const path = require('path');
 
 const publicPath = path.join(__dirname, 'public');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: publicPath,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: publicPath,
-    historyApiFallback: true
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: publicPath,
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }]
+    },
+    devtool: isProduction ? 'source-map': 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: publicPath,
+      historyApiFallback: true
+    }
   }
-}
+};
