da7acc0ec481e7e6cca86b64c9289ec81463cbd1
Anthony Zeng <anthonyzeng99@gmail.com>
2018-06-14 18:07:58 -0400

    finished lesson 165

--- 02expensify/src/store/configureStore.js
+++ 02expensify/src/store/configureStore.js
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 // Reducers
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
+import authReducer from '../reducers/auth'
 
 const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -11,7 +12,8 @@ export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
-      filters: filtersReducer
+      filters: filtersReducer,
+      auth: authReducer
     }),
     composeEnchancers(applyMiddleware(thunk))
   );
